99dadbc88fd44a2e6dbb87938cd66b36e71f2004
Miguel Angel Ortiz <ortiz.garcia.miguel.angel@gmail.com>
2017-12-05 19:03:13 +0100

    Commit inicial

---
+++ app/src/main/java/xyz/maog/tresenraya/MainActivity.java
@@ -0,0 +1,13 @@
+package xyz.maog.tresenraya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
